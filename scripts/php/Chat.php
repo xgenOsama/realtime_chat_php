495703149efa05f38369c1f8381534724d275ed1
gen <xgen.osama@gmail.com>
2015-06-09 17:07:10 +0200

    final commit

--- scripts/php/Chat.php
+++ scripts/php/Chat.php
@@ -5,6 +5,6 @@
 
 $messages = get_msg();
 foreach($messages as $message){
-    echo '<strong>'.$message['sender'].'</strong> send <br/>';
-    echo '<pre>'.$message['message'].'</pre> <hr/>';
+    echo '<strong style="color: crimson">'.$message['sender'].'</strong> send <br/>';
+    echo '<pre style="color: cadetblue">'.$message['message'].'</pre> <hr/>';
 }
